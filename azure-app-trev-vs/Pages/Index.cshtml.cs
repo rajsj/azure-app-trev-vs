98062e0f7ba366bca8fdd3de82f44112cc5f9a85
rajsj1911@outlook.com <rajsj1911@outlook.com>
2026-05-10 15:51:58 +0530

    Added configuration message

--- azure-app-trev-vs/Pages/Index.cshtml.cs
+++ azure-app-trev-vs/Pages/Index.cshtml.cs
@@ -4,9 +4,20 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace azure_app_trev_vs.Pages
 {
     public class IndexModel : PageModel
+
     {
+        private readonly IConfiguration _configuration;
+
+        public IndexModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string Greeting { get; private set; }
+
         public void OnGet()
         {
+            ViewData["Greeting"] = _configuration["Greeting"];
 
         }
     }
